d3e243ab5c217290bb42f3d8b379d067d8bedd7e
sirmedina <douglasmedina@wimpublicidade.com>
2015-05-03 13:17:54 -0300

    adding comments

---
+++ first/db/migrate/20150503161045_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+	def change
+	    create_table :comments do |t|
+		    t.string :user_name
+		    t.text :body
+		    t.integer :idea_id
+
+	    	t.timestamps null: false
+	    end
+    	add_foreign_key :comments, :ideas
+	end
+end
